6f8c8fbbffb830c270f5529b9c51a754ea9dff28
Valentin Bryukhanov <bvv.mag@gmail.com>
2015-04-07 09:18:49 +0300

    Updates

--- verification/src/referee.py
+++ verification/src/referee.py
@@ -6,7 +6,7 @@ from tests import TESTS
 
 
 class Referee(RefereeCodeGolf):
-    DEFAULT_MAX_CODE_LENGTH = 150
+    DEFAULT_MAX_CODE_LENGTH = 120
     BASE_POINTS = 10
     TESTS = TESTS
     ENVIRONMENTS = settings_env.ENVIRONMENTS
